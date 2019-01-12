1fd9e0b6f4d3b43e80ca5dd90fbe620e69353457
ArthurKwok <951224758arthur@gmail.com>
2019-01-12 17:39:50 +0800

    new

--- video_split.py
+++ video_split.py
@@ -19,10 +19,12 @@ if __name__ == '__main__':
     frame_num = 1
     clip = cv2.VideoCapture(video_path)
     if clip.isOpened():
+        print("read video")
         rval, frame = clip.read()
     else:
         rval = False
     while rval:
         rval, frame = clip.read()
+        print("Saving frame {:0>5d}".format(frame_num), end='\r', flush=True)
         cv2.imwrite(path.join(frame_path, "frame{:0>5d}.png".format(frame_num)))
 
