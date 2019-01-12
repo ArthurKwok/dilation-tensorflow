b524272ddbe780d687b3943fa7e90360d0936f82
ArthurKwok <951224758arthur@gmail.com>
2019-01-12 17:36:14 +0800

    new

---
+++ video_split.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import pickle
+import cv2
+import os
+import os.path as path
+from utils import predict
+from model import dilation_model_pretrained
+from datasets import CONFIG
+import glob
+import numpy as np
+
+if __name__ == '__main__':
+
+    video_path = './data/test_video.mp4'
+    frame_path = './data/video_frames/'
+
+    params = list()
+    params.append(cv2.CV_IMWRITE_PNG_COMPRESSION)
+    params.append(8)
+
+    frame_num = 1
+    clip = cv2.VideoCapture(video_path)
+    if clip.isOpened():
+        rval, frame = clip.read()
+    else:
+        rval = False
+    while rval:
+        rval, frame = clip.read()
+        cv2.imwrite(path.join(frame_path, "frame{:0>5d}.png".format(frame_num)))
+
