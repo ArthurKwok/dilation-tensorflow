7077081b3ce8f7f662aabaebeeaa1bd8f533b98c
ArthurKwok <951224758arthur@gmail.com>
2019-01-12 19:33:19 +0800

    new

--- video_merge.py
+++ video_merge.py
@@ -10,17 +10,17 @@ if __name__ == '__main__':
     image_folder = './data/video_frames/'
     video_name = 'video.avi'
 
-    images = [img for img in sorted(os.listdir(image_folder)) if img.endswith(".png")]
-    frame = cv2.imread(os.path.join(image_folder, images[0]))
-    height, width, layers = frame.shape
+    # images = [img for img in sorted(os.listdir(image_folder)) if img.endswith(".png")]
+    # frame = cv2.imread(os.path.join(image_folder, images[0]))
+    # height, width, layers = frame.shape
 
-    video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+    # video = cv2.VideoWriter(video_name, 0, 1, (width,height))
 
-    for image in images:
-        video.write(cv2.imread(os.path.join(image_folder, image)))
+    # for image in images:
+    #     video.write(cv2.imread(os.path.join(image_folder, image)))
 
-    cv2.destroyAllWindows()
-    video.release()
+    # cv2.destroyAllWindows()
+    # video.release()
 
     paths = os.listdir(image_folder)
     print(paths)
